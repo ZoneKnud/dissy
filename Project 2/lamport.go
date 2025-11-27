eef4b2521f2075587d7f3dba57bdbe8527913981
josefinepeltola <202306492@post.au.dk>
2025-11-27 20:17:15 +0100

    fixed comments

--- Project 2/lamport.go
+++ Project 2/lamport.go
@@ -5,22 +5,20 @@ import (
 	"sync"
 )
 
-// LamportClock er en simpel logisk ur implementation
-// Den holder styr på en enkelt counter værdi
+// Lamport timestamp struct initialization
 type LamportClock struct {
 	time  int       // Den logiske tid
 	mutex sync.Mutex // Sikrer at kun én goroutine ad gangen kan ændre time
 }
 
-// NewLamportClock opretter et nyt Lamport ur der starter ved 0
+// Opretter et Lamport ur med tid=0
 func NewLamportClock() *LamportClock {
 	return &LamportClock{
 		time: 0,
 	}
 }
 
-// LocalEvent kaldes når processen udfører en lokal operation
-// Den øger den logiske tid med 1
+// Udfører en lokal operation
 func (lc *LamportClock) LocalEvent() int {
 	lc.mutex.Lock()         // Lås så andre ikke kan ændre samtidig
 	defer lc.mutex.Unlock() // Unlock når funktionen er færdig
@@ -29,8 +27,7 @@ func (lc *LamportClock) LocalEvent() int {
 	return lc.time
 }
 
-// SendEvent kaldes før en besked sendes til en anden proces
-// Den øger tiden og returnerer timestamp der skal sendes med beskeden
+// Send event, increment time counter
 func (lc *LamportClock) SendEvent() int {
 	lc.mutex.Lock()
 	defer lc.mutex.Unlock()
@@ -39,38 +36,34 @@ func (lc *LamportClock) SendEvent() int {
 	return lc.time
 }
 
-// ReceiveEvent kaldes når en besked modtages fra en anden proces
-// Den synkroniserer uret med afsenderens tid ved at tage max af de to
-// Dette sikrer kausal ordering: hvis event A -> B, så time(A) < time(B)
+// Sammenlign modtaget time med egen tid, vælg max 
 func (lc *LamportClock) ReceiveEvent(receivedTime int) int {
 	lc.mutex.Lock()
 	defer lc.mutex.Unlock()
-	
-	// Find den største af vores tid og modtaget tid
+
+	// Find max 
 	if receivedTime > lc.time {
 		lc.time = receivedTime
 	}
-	
-	// Øg med 1 for receive event
 	lc.time++
 	return lc.time
 }
 
-// GetTime returnerer den nuværende logiske tid (bruges til debugging)
+// Retuner tid
 func (lc *LamportClock) GetTime() int {
 	lc.mutex.Lock()
 	defer lc.mutex.Unlock()
 	return lc.time
 }
 
-// LamportMessage repræsenterer en besked med Lamport timestamp
+// Lamport message struct initialization
 type LamportMessage struct {
 	Timestamp int    // Lamport tiden når beskeden blev sendt
 	ProcessID int    // ID på den proces der sendte beskeden
 	Content   string // Selve besked-indholdet
 }
 
-// String gør det nemt at printe en LamportMessage
+// Print funktion
 func (msg LamportMessage) String() string {
 	return fmt.Sprintf("P%d@T%d: %s", msg.ProcessID, msg.Timestamp, msg.Content)
 }
--- Project 2/vector.go
+++ Project 2/vector.go
@@ -6,18 +6,14 @@ import (
 	"sync"
 )
 
-// VectorClock er en mere avanceret logisk ur implementation
-// Den holder et array af counters - én for hver proces i systemet
-// Dette gør det muligt at opnå total ordering af events
+// VectorClock struct
 type VectorClock struct {
-	vector    []int      // Et array hvor index er processID og værdi er dens tid
-	processID int        // Denne proces' ID
-	mutex     sync.Mutex // Sikrer thread-safety
+	vector    []int
+	processID int
+	mutex     sync.Mutex
 }
 
-// NewVectorClock opretter et nyt Vector clock
-// numProcesses: hvor mange processer der er i systemet
-// processID: ID for denne specifikke proces (0-indexed)
+// Opretter et nyt Vector clock
 func NewVectorClock(numProcesses int, processID int) *VectorClock {
 	vector := make([]int, numProcesses) // Lav et array med 0'er
 	return &VectorClock{
@@ -26,47 +22,41 @@ func NewVectorClock(numProcesses int, processID int) *VectorClock {
 	}
 }
 
-// LocalEvent kaldes når processen udfører en lokal operation
-// Den øger kun denne proces' egen counter i vectoren
+// Processen udfører en lokal operation
 func (vc *VectorClock) LocalEvent() []int {
 	vc.mutex.Lock()
 	defer vc.mutex.Unlock()
-	
+
 	vc.vector[vc.processID]++
 	return vc.getCopy()
 }
 
-// SendEvent kaldes før en besked sendes
-// Den øger denne proces' counter og returnerer hele vectoren
+// Send event, increment proces counter og returnerer hele vectoren
 func (vc *VectorClock) SendEvent() []int {
 	vc.mutex.Lock()
 	defer vc.mutex.Unlock()
-	
+
 	vc.vector[vc.processID]++
 	return vc.getCopy()
 }
 
-// ReceiveEvent kaldes når en besked modtages
-// Den merger denne vector med den modtagne vector:
-// For hver position i vectoren, tag max(vores værdi, deres værdi)
-// Dette sikrer at vi ved besked om alle events der skete før denne besked
+// Merge proces' vector med recieved vector
 func (vc *VectorClock) ReceiveEvent(receivedVector []int) []int {
 	vc.mutex.Lock()
 	defer vc.mutex.Unlock()
-	
+
 	// Merge: tag maximum af hver position
 	for i := 0; i < len(vc.vector); i++ {
 		if receivedVector[i] > vc.vector[i] {
 			vc.vector[i] = receivedVector[i]
 		}
 	}
-	
-	// Øg vores egen counter
+
 	vc.vector[vc.processID]++
 	return vc.getCopy()
 }
 
-// getCopy returnerer en kopi af vectoren (skal allerede være locked)
+// Returnerer en kopi
 func (vc *VectorClock) getCopy() []int {
 	copy := make([]int, len(vc.vector))
 	for i := 0; i < len(vc.vector); i++ {
@@ -75,25 +65,22 @@ func (vc *VectorClock) getCopy() []int {
 	return copy
 }
 
-// GetVector returnerer en kopi af den aktuelle vector
+// Retuner aktuel vector
 func (vc *VectorClock) GetVector() []int {
 	vc.mutex.Lock()
 	defer vc.mutex.Unlock()
 	return vc.getCopy()
 }
 
-// Compare sammenligner to vector clocks og returnerer deres relation
-// Returnerer: -1 hvis v1 < v2 (v1 happened before v2)
-//             1 hvis v1 > v2 (v2 happened before v1)
-//             0 hvis de er concurrent (ingen kausal relation)
+// Sammenlign vectors og find relation
 func CompareVectors(v1, v2 []int) int {
 	if len(v1) != len(v2) {
 		panic("Vector clocks skal have samme længde!")
 	}
-	
-	lessOrEqual := true  // Er v1 <= v2?
+
+	lessOrEqual := true    // Er v1 <= v2?
 	greaterOrEqual := true // Er v1 >= v2?
-	
+
 	for i := 0; i < len(v1); i++ {
 		if v1[i] > v2[i] {
 			lessOrEqual = false
@@ -102,34 +89,33 @@ func CompareVectors(v1, v2 []int) int {
 			greaterOrEqual = false
 		}
 	}
-	
+
 	// Hvis v1 <= v2 og mindst ét element er mindre
 	if lessOrEqual && !greaterOrEqual {
 		return -1 // v1 happened before v2
 	}
-	
+
 	// Hvis v1 >= v2 og mindst ét element er større
 	if greaterOrEqual && !lessOrEqual {
 		return 1 // v2 happened before v1
 	}
-	
+
 	// Hvis v1 == v2
 	if lessOrEqual && greaterOrEqual {
 		return 0 // De er identiske (samme event eller concurrent)
 	}
-	
-	// Ellers er de concurrent
+
 	return 0
 }
 
-// VectorMessage repræsenterer en besked med Vector clock timestamp
+// Vector besked med timestamp
 type VectorMessage struct {
 	Timestamp []int  // Vector clock når beskeden blev sendt
 	ProcessID int    // ID på den proces der sendte beskeden
 	Content   string // Besked-indhold
 }
 
-// String gør det nemt at printe en VectorMessage
+// Print funktion
 func (msg VectorMessage) String() string {
 	// Konverter vector til string format: [1,2,3]
 	vectorStr := "["
@@ -140,11 +126,11 @@ func (msg VectorMessage) String() string {
 		vectorStr += fmt.Sprintf("%d", v)
 	}
 	vectorStr += "]"
-	
+
 	return fmt.Sprintf("P%d@%s: %s", msg.ProcessID, vectorStr, msg.Content)
 }
 
-// FormatVector laver en pæn string representation af en vector
+// Print funktion 
 func FormatVector(v []int) string {
 	parts := make([]string, len(v))
 	for i, val := range v {
